b768218f534a46a378c24af280030f564235e015
Krzysztof Nowak <k.nowak@cern.ch>
2017-01-20 09:02:35 +0100

    Added deploy key

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,24 +1,39 @@
-mv content/ content_root/
 cd databags
 mv navigation.json navigation_local.json
 ln -s navigation_deploy.json navigation.json
 cd ..
 
+eval $(ssh-agent -s)
+mv content/ content_root/
 ln -s content_root/help content
+
+openssl aes-256-cbc -K $encrypted_1c9ebd4878cf_key -iv $encrypted_1c9ebd4878cf_iv -in deploy_keys.tar.enc -out deploy_keys.tar -d
+tar xvf deploy_keys.tar
+
 lektor clean --yes
 lektor build
-lektor deploy ghpageshelp
+ls -l
+ssh-add -D
+ssh-add ./deploy_key_help
+lektor deploy ghpageshelp --key-file ${TRAVIS_BUILD_DIR}/deploy_key_help
 rm content
+
 ln -s content_root/about content
 lektor clean --yes
 lektor build
-lektor deploy ghpagesabout
+ssh-add -D
+ssh-add ./deploy_key_about
+lektor deploy ghpagesabout --key-file ${TRAVIS_BUILD_DIR}/deploy_key_about
 rm content
+
 ln -s content_root/blog content
 lektor clean --yes
 lektor build
-lektor deploy ghpagesblog
+ssh-add -D
+ssh-add ./deploy_key_blog
+lektor deploy ghpagesblog --key-file ${TRAVIS_BUILD_DIR}/deploy_key_blog
 rm content
+
 mv content_root/ content/
 rm databags/navigation.json
 mv databags/navigation_local.json databags/navigation.json
