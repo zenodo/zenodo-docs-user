efaa83bf8b6c6d8684f02d32cff6b0947b6caa75
Alexander Ioannidis <a.ioannidis@cern.ch>
2021-02-15 18:21:31 +0100

    global: replace TravisCI with GH actions

--- rundeploy.sh
+++ rundeploy.sh
@@ -1,4 +1,6 @@
-# Copyright (C) 2017 CERN.
+#!/usr/bin/env bash
+#
+# Copyright (C) 2017-2021 CERN.
 #
 # Zenodo is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -23,35 +25,25 @@ mv navigation.json navigation_local.json
 ln -s navigation_deploy.json navigation.json
 cd ..
 
-eval $(ssh-agent -s)
 mv content/ content_root/
 ln -s content_root/help content
-
-openssl aes-256-cbc -K $encrypted_1c9ebd4878cf_key -iv $encrypted_1c9ebd4878cf_iv -in deploy_keys.tar.enc -out deploy_keys.tar -d
-tar xvf deploy_keys.tar
 
 lektor clean --yes
 lektor build
 ls -l
-ssh-add -D
-ssh-add ./deploy_key_help
-lektor deploy ghpageshelp --key-file ${TRAVIS_BUILD_DIR}/deploy_key_help
+lektor deploy ghpageshelp
 rm content
 
 ln -s content_root/about content
 lektor clean --yes
 lektor build
-ssh-add -D
-ssh-add ./deploy_key_about
-lektor deploy ghpagesabout --key-file ${TRAVIS_BUILD_DIR}/deploy_key_about
+lektor deploy ghpagesabout
 rm content
 
 ln -s content_root/blog content
 lektor clean --yes
 lektor build
-ssh-add -D
-ssh-add ./deploy_key_blog
-lektor deploy ghpagesblog --key-file ${TRAVIS_BUILD_DIR}/deploy_key_blog
+lektor deploy ghpagesblog
 rm content
 
 mv content_root/ content/
--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,6 @@
-# Copyright (C) 2017 CERN.
+#!/usr/bin/env bash
+#
+# Copyright (C) 2017-2021 CERN.
 #
 # Zenodo is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
