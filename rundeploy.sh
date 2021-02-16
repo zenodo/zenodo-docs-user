271de0d2738dea61d292f473446da5feec7c0230
Alexander Ioannidis <a.ioannidis@cern.ch>
2021-02-16 12:29:58 +0100

    gh-actions: remove ssh-agent action step

--- rundeploy.sh
+++ rundeploy.sh
@@ -25,21 +25,36 @@ mv navigation.json navigation_local.json
 ln -s navigation_deploy.json navigation.json
 cd ..
 
+# Start ssh-agent
+eval $(ssh-agent -s)
+
+# Make sure to kill the ssh-agent process on EXIT
+trap "kill -9 $SSH_AGENT_PID" EXIT
+
 mv content/ content_root/
 ln -s content_root/help content
 
+# Setup and build help pages
+ssh-add -D
+ssh-add - <<< "${HELP_SSH_PRIVATE_KEY}"
 lektor clean --yes
 lektor build
 ls -l
 lektor deploy ghpageshelp
 rm content
 
+# Setup and build about pages
+ssh-add -D
+ssh-add - <<< "${ABOUT_SSH_PRIVATE_KEY}"
 ln -s content_root/about content
 lektor clean --yes
 lektor build
 lektor deploy ghpagesabout
 rm content
 
+# Setup and build blog pages
+ssh-add -D
+ssh-add - <<< "${BLOG_SSH_PRIVATE_KEY}"
 ln -s content_root/blog content
 lektor clean --yes
 lektor build
